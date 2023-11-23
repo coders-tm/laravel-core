09edbbd1c131b4d8191aa3c2e3485fecff5d0925
Dipak Sarkar <hello@dipaksarkar.in>
2023-11-23 15:01:43 +0530

    updated

--- src/Providers/CoderstmEventServiceProvider.php
+++ src/Providers/CoderstmEventServiceProvider.php
@@ -1,85 +1,53 @@
-<?php //ICB0 81:0 82:1183                                                     ?><?php //00a01
-// IONCUBE ENCODER 13.0 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+<?php
 
-// *************************************************************************
-// *                                                                       *
-// * Laravel Core - Core package that provides core functionalities        *
-// * Copyright (c) Coderstm. All Rights Reserved,                          *
-// * Version:v2.0                                                          *
-// * Build Date: 2023-11-22                                                *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * Email: hello@coderstm.com                                             *
-// * Website: https://coderstm.com                                         *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * This software is furnished under a license and may be used and copied *
-// * only  in  accordance  with  the  terms  of such  license and with the *
-// * inclusion of the above copyright notice.  This software  or any other *
-// * copies thereof may not be provided or otherwise made available to any *
-// * other person.  No title to and  ownership of the  software is  hereby *
-// * transferred.                                                          *
-// *                                                                       *
-// *************************************************************************
-if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
-?>
-HR+cPzVDbBKC46swkNu1biLyk7+2yU73FRK7NvJ8hEj/BM/FPebLsavTMsg1Y7YnLdoq6yDZNUkF
-G7C/sYeEV28lC9VfC2goNaq6OCsj7yDxN/dG1cScc5gL6q+VBUYKAAq+J6zdSQOpH8QY/izrixWl
-FuUpENGlhO8WanY5XmyaeWefJRQ0wBzcTAJtbV/iDRiOyYc9bHx1UxfxT1xup93GwzXSZb8p9Zyl
-/T4YKJITE2tB3Kf6OJYlacnmkB3tgnN5dn9ynUkqrBEbT0h9YGd4QSvinz6nHzyD6OLTpZVA6P+n
-inKSR/8B48+GH4m7gXHqOL4BR/+dX6/xxy2vU3S+/BrszyFA1VNA4B6LBi9DBF2cYBYZpD3Rpw5M
-O1ICmaHP6Hp58SQIWfobB8BEwceP9ej8TB3SriNO2bob2DRrVSLDNuBylHdCme/iL6v4VmJT+Sdp
-C3A0qzwPBSfPwDIz2mYvdmSXlx5X3SJrBKYxXaeMIIxv9UaQ5YBGWeXkXq9Mu1/2taEhPlXF4vao
-wZ0wyJ815LcVooP6zIRk0Ny9cR4aJI10NWoPW2MwJj+pj6vZsgDPcloWTzEpRrmbdlwnEkcK3Mvw
-uBHDirQ8vvYP2XjCKPXFoed2eUCQyedfEuRAnQ0a1PyNHSTR00FuGG27hRB4tdWLvqO/zsGMgO9C
-SiD/XQctOGasNbtxp+RVXwAAc1OQ4BhXL0h8nDfYxKh3kKscEY3g2mI4Yzl6D1j4/pD6IF303Cne
-0yWTwYsaTWRxiJZK8Piruh8+NUVo8J1EXjeQBMRF8MKnM7RQyb/wNmeseTfu3gavcCRBTUNSuSCr
-x2Z51v8RY3XYZ2Z0PRNPiFSiMGYLRN86OJOao5MbzrHhQAuYpNp2kRWT3xDKB3uz/qnieFfa7ojZ
-TXYPKs1zqzmIxQfJCk/QR6zL79e0vlaKD1IGJHB4c25ttjotvU1CZhIB6a6oVbKOVWQaefMf8nVw
-asO73TQeHXZKJH5KEYLo8YBhZLJl5W0bP9nYBkRHSQRSyDTn9QvcnQPS5pgo8+b/Ldl3JaHJCk88
-OOcb4u0g1XsTcjrLtxbyiqVMYlF1AcbWysILHoIVCK/Dp8tu5ORAPhkB6bcqgslufsDUZNoyOd/t
-W9mz791gNHFR6YOMhB8b8af2ljY8+RPnSzppcyIWWGMCZ4vaZUhdujwfequPrw4LEsiC7SFVTU/R
-u7GPHYgsZh5AD5Asd7lEe0QYohO+3bL+//9SD+AyEbt11IqaEwCu9c2j9fMv4A4mLMB6RQWJcfDr
-1aauI+OtGkeNwFwKB7QzUZzUS2iTwSy3ZPIFV8aHQ9woP7F9DGImPgl64TbeRgX7gigjWfew3iBm
-M3MPIeLplXSMimbf6H/LEEzm6DJhr9BQSRbNu3MZ3AszNRvLyvSfGZKJj6G+rhQUpFHLEE1SFfY4
-6I7zHJwANPQrZmgOKFT2NrCniX4taNnLakOaXZDDH961tVIPL4sdf1kXt0dT3wniX3f9JAv/gjJJ
-dgn3GgeUesgsdNvSQkhZu6+sP9bXceCmpmoXS20z/th8PWOMyDtokdTMUAGJrsW5MEB/9W1jh8Ax
-Ge64OFkP4u5P7DJW/zybEmCV0rmHP9gsWtMswd3vFdUrrpAtvbDFq8zdnyo6mO/Jx4N2G3Z6n0PA
-yn2iYwKhoutz6s6kqfwb+EgMia4qs8DbRIzli2N2hZJObf0vUjIw0E0FSbcb72KhhRouPmhu2tT2
-f9jZ9HCSpu/WcjnStXIVXj8IdrcG8qkISo0DWAfKHmN/EdncJM/6HGFCGTaTH+JNW2aYT715jBt1
-X6ZoLkDruhgx/cRnLayKa66mcrwhG3bg/6lBbIB0d8mmmhrnklUqLJg17Py8fxr0PZe==
-<?php //000a3
-// IONCUBE ENCODER 13.0 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+namespace Coderstm\Providers;
 
-?>
-HR+cPxrZ73EujEG4DTc94BCFrW4jOViTNtPHq+YOxXPde7dLBoWN9Si5fSVk8Yf/tL8uXUl1Cdgw
-oh3PqFYhJ1apsbPFUU1NkKsmPWyl0bbQVf9o1hdunnqqGknU3KFbLcry7l0+l6YGySQlZOxdj7Qu
-u6gzoV3OzsYxvlqkGTUJADRXLxVx7W817AhwCxf0oUg2XTjL2JIb6P49pt8AFI0Hr15XrgBUmxHH
-mDaRLWhxiIOkdJATBxErNl192Wd/L2YenPEE17Xp8kjM31i4FUiK+Ns26dIvE6Q1XHwknDak0wqS
-Z7k1ccws9A4NDiDupt29T25YAn//od4jy4jefU3ZT0XpEivdFMGmiH9ChLW+9xj+AOn0YO/fQay5
-H/rkBFw1xphkdC8Hu6kmZMc+nh17Ijdch11MmUgGvcFQlbpMkLzLhlK3VsHWaIz0OTIbuWL72B7n
-OIW/O5H6DBtyd6orgnOoaKC8a3g303kQ48RxW+JHKGlcTNT1DDHJqSe/pbcPtXjik2XmffE/kk9y
-zsJeKxv95EkmRr0xM4HEskBMkjDpDKZxin5GFeMdUhDlUwNThQaFAv5nsbtPavBqQhZKhBPJ4kU+
-wJO5mtRWbpkAqMp7/IM8a5pAKjgWR4JOB2od6Sg7DAlfJ+RaoScXuHcuzQy8brClRV/l/7mEpGFW
-ZqtRh/IS1PV2cpJzOqS2mPUp502uWXNmy43M73avYcExthyw34WET6+LKalOyFGFvC6H5jHBn/2+
-OpsM28KcWAmfDrD90MtbZDN4sdD1hBQiGYWkbooWYTqsamrutdJep/ytT2wy2GB9/r60JzEFC9cW
-dDGYf/KVZaM6lsRG8EY1f+thh8aUdZPy76pDHySRJPfHljyoP3EB+bMXvivQiniFJhXOZnyvHg+4
-bg3oNbVUfqFx7/r38+WDtGt95S3vRRUnwcn8Q8MeJstPEthheDqwpzbidNhiJkckcLCZZ1WjoL/R
-aPs26Gl9qKQ7t36i7gXdiN8J02nc/oBwdAN/n7Urn/aFoNZ12ezHbD3d3Vfp4mijGcm0jL2u3SAo
-X7frQcLlnMa/SD4e+ed5yWWFs/2f6NtCeFimGPU5bGriQCkyOMfzS80sntcu19V66XCUKJWVsIyA
-+q5nLfyk6y1sno7YbofclqK3kv/utpj2ClcXbdsjXc+BLOqYX/HdzqfTbeERUqi5nSDllyycMraa
-8E30EyWNqlnSRq3GRnqr8BSlNG39CDcx8vxxds89gMnzl+3R+N65KIHhReaL559ygNrTQrfJtOw9
-p7fj4AA97hfvgCe9HvmUyAEtODg9MlMCU/PM8Mk6UMrijr8zJgSZOYjE6H8lYnj115//SBpfO6VX
-JZu7X+DRCaGQViY2DlpG3CAqhTjXv17gFPf0QUBeWUkj9r/Ezg/PnHXOLL6+JBxBjtJtFIJWZv/z
-2gOfC7Wv/AopDQOumnFvzHvsw2ZJxQ5ChuEB3CBrFTnVuQ235r/ROL+GBSy4wcawB8BX0FiCXt+Q
-7TrliaUGHg6QU6O2VZC7H1xEzxku9tJyLJH+7bYt77Fq6DwKTiV9vtXqLma+7Hup/NIUNap9vqdr
-94F870o7fSzBV1/EwV4MlN0hdgl1qGIiUSfaGUl+6xYFfuMVqtSUzeIk7ViU4WkxHVAxfrLSFRSC
-N4krZ8GD8J+KMVqYdYsp7i2setU0IZ8HkooOkwoGR5HZIQT/CnSvf2Juxq+G4ylf7AgLY7VJAvOB
-mdY7MVsYxe2Izdamx2wqgehNS5RRPgQl02clsoHd9UF92MzxQ8gRRWvmDz+RnHxFo513xxGl+cvj
-BjzHP5FXCGWflFUnuoBftin3+mvHRruXZOF/w/3rLzT4FYw/tCFNMyG8yOWG6N96jwgnJl2/
+use Coderstm\Events\TaskCreated;
+use Coderstm\Events\EnquiryCreated;
+use Coderstm\Events\UserSubscribed;
+use Laravel\Cashier\Events\WebhookReceived;
+use Coderstm\Listeners\SendSignupNotification;
+use Coderstm\Listeners\SendEnquiryConfirmation;
+use Coderstm\Listeners\SendEnquiryNotification;
+use Coderstm\Listeners\SendTaskUsersNotification;
+use Coderstm\Events\Cashier\SubscriptionProcessed;
+use Coderstm\Listeners\Cashier\CashierEventListener;
+use Coderstm\Listeners\Cashier\SubscriptionEventListener;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class CoderstmEventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        EnquiryCreated::class => [
+            SendEnquiryNotification::class,
+            SendEnquiryConfirmation::class,
+        ],
+        TaskCreated::class => [
+            SendTaskUsersNotification::class,
+        ],
+        WebhookReceived::class => [
+            CashierEventListener::class,
+        ],
+        SubscriptionProcessed::class => [
+            SubscriptionEventListener::class,
+        ],
+        UserSubscribed::class => [
+            SendSignupNotification::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
--- src/Providers/CoderstmPermissionsServiceProvider.php
+++ src/Providers/CoderstmPermissionsServiceProvider.php
@@ -1,125 +1,49 @@
-<?php //ICB0 81:0 82:175f                                                     ?><?php //00a01
-// IONCUBE ENCODER 13.0 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+<?php
 
-// *************************************************************************
-// *                                                                       *
-// * Laravel Core - Core package that provides core functionalities        *
-// * Copyright (c) Coderstm. All Rights Reserved,                          *
-// * Version:v2.0                                                          *
-// * Build Date: 2023-11-22                                                *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * Email: hello@coderstm.com                                             *
-// * Website: https://coderstm.com                                         *
-// *                                                                       *
-// *************************************************************************
-// *                                                                       *
-// * This software is furnished under a license and may be used and copied *
-// * only  in  accordance  with  the  terms  of such  license and with the *
-// * inclusion of the above copyright notice.  This software  or any other *
-// * copies thereof may not be provided or otherwise made available to any *
-// * other person.  No title to and  ownership of the  software is  hereby *
-// * transferred.                                                          *
-// *                                                                       *
-// *************************************************************************
-if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
-?>
-HR+cPr2n5PZy5UxVYNc0kwokVDunNv8uwfM2xkmwyZGNpWPjHwpUN7V4jqT3Pisu24WUZvHYgQ9D
-ltgeZrxoWi1CnKZ8rYEWpS/QgspfYlOZ9A7Eoo20T9kDdKQA3utGOxIoY0f/87NAxxSWC+0QocnR
-mNS++4pjm+s559FzpRpkzvTOUDhMRg8z8LY9m67VXdCMfA9/CMo6WdfPLZO9rvxk33rOA2qKJzIy
-W4omloDa7DNve59uh4Auo+NcnA2YZoLaZrWMgX/sRsnPpLIhsROH9pe7yS/HiKVV3Hc5NSutoXcV
-iRCLn7Pt6DNqVRZedgdm125G2ps67B3H3ids77b6RuUQaAmiFTg7uwLUHeO5cCxuG6GUrtU/CnXH
-ogm9WJMFW1T417m3QpNqgbtot3LLmvoVRsZphs3h7UEUJMh6Ng64nLEGoquc1T41LeebHhe+ENso
-zoSAAwJRTX2ssUmAnZI2/04o0h1DUiLxUUFHIdw5ih0Da1V2a0lLFr2TnpPuBx0bMdbw3OzNet9W
-/68r72+DnIzYyK7YMRks8R08tPEt1bZVTwkSxgiv3bNfk4UNyT7wOC6z5YUox5Kj6vf3qMcjMNLr
-4cvKMadwM4guXy6kUlcjcUAR6qF/rbMC+M/S7Iw1T5ZlH5muwFpDz9dISVKPwXzWKZOl0nh2T9Pw
-Db2DemZdnIV2hyv+vP/mjz2mFOx0q8FGJ6nmjFTCqPiHBiaTevevSE8wlwjr4LQJyIDer4XLougJ
-lr/4f4jFP/qVMJbbGTOdRoPr9PfLex0GUUkK21KeeOLpgudljpDoma56YovHLBLz0gLoIyyI1mDT
-a8iizrA3mMpiAk76V7Jugtev8WECqJu85+xW5RygrN6UpdrUR8GNyAorqp2lDwz5z3wnPpsSPnMT
-norIjW3As0z4CZICc0Rv3GrgT4KRy/6+s3rW+KDH8fcgvzNzDdin3goBqQv3dTILfz642hyHBtcZ
-vbZPQp3KANKcRYRy4Ae3MfqU4W+OPLV8MzuH4ijlVNZLhnTaO/+uJkRQSfa8ndb4Jtj95FODsSNo
-gGaVUijyt8kywJdGfnbyVP/dK87icm9dvxepReecfr24DFrsnp0XbLpwKaqefRvXzzk1qgJqhiPl
-Mu8eMDSakCpV3ws9qld6OGCcpOxCjeIgBvkuT1JgqS1ntNpf9wJDlHLlWhTqRYpYYwCtklqQ7f6q
-Bfo/CYBMthzlrqnVIhcspY/BrT8fRVA4xj9YjLJ89nmYGzXfoNDlMeEDLIsSrYOrmeXZHZuVRg6n
-0rXxwPLwSOIwi3wS25H3c4ucBnRP23f4scGDWUdMbMin2lEnICyvxXDW/4Qkdb9SSHujmHSL4COo
-oUG1ft2HalsZG103JymOa3OrQFWQvEQreIo7CY/ZZpYxV4c/OE2uP6lw3OO7qIcLQSPeeFM+Uv2m
-FfeAvFDZ8yLaYC9CZXgDWHupG8tXPxpMGfJsj3aDs8GHJ/8rTMuPioE8vajpmPxCAo4HQ7oycaRY
-7gAq2qgskobRbHj0JbP94lF9OIiEAgtzRi5JHD+MbOmqWALGgQdkL98Wo2x6xMJ8PRFlC2vLgmdn
-hY30gt4QaUZdNEMsalddfiMJxE8kD8Uo3uvGVkmuOE/rtu3x8KDSTeforQPaDsB9KZXRXQrOKngf
-VKlER94wiU0xkYqwsQR/QXcakX6+1uMnzC5tPNTvK4E/6yKX1/V+DpHXykHZdCjEF/y31Uugx9Kh
-Xo6S+XnbPrTdr6THO0j4aZW0OF8mtJGzUlcXfCtSRYDGrhK6+FsPLv22kefTeltHKkZksG7jnOhf
-HGqEs4ubInSbTvA+wyIfxBnBDRgEZ43PjrCppK8UzDLelGEujbVR8EtschmXP79XqrrGL8Y7UZBZ
-vLfdKCaNx1OraOzE88QrRqpd/vDGEfe3ACM5uwza6L+QrKsV+Hm3dKpjQgdgJek4QzvxPNYtJqBp
-zrLXSfxxI6/rf+mYjZAP4LsXvpPoQEKbqpr69ki3JK39Gov9bLzxEv6nZ1VmQOtTqnpAe0SQbDAc
-MJNJwCcWQmfInTe2P/x1x5jUS7zY/uHOUTdTMixYjec0diAlEz3GzAbhl9+Yikfjs1aXO6ZCN/TD
-3T6oiM4sTHR2lpt/+6u4/A0WMfSjLZIqzSLf3Thvdc2z4arFw24DM41BTwMBCHyBN5HfEe7o4Kw9
-/X2mjwEwz3exKiajGL0n2ABWCEsTvky2eZvAnLl7J9LwRQsDnbEix/cqpe7+AIqBL06spjMD5ee5
-10u52+BtwLseeeetBcb1QT2r6qrZt9XRfaq02COfXCHVuSjs5dSbG8Hyc0bS5uHbeP1TMFjvaqPo
-wt0cTWvXp4zJtLHXrh37Bz0PZXWjJ7xju3Kp5m7+D7T+Ssq5wa9SeBMa2AYvK+T20mewkvxjNn5I
-hVSodlzpkKoI+aiTVQzm/HLbjCsbNKitEkzJTjoBJNW9/Kcf0LNMFokepCrjT+sthHcjHOgKPusg
-ql0YuSJRQ42PCIXayUAiILQ86KW4aQz3/EI2WH2+YC4AEi29BXtyg4Pv6Ut/o75JhGRkmGVs6cQd
-87IUjQ7I8nVYkzobQ1V5wg49ydWZyMR1ijDOz1TmikSvk6OGnAfXKpBNdGmUeI56J2UkXhWx3z1V
-g7Uy/Esp/3/8CYCG2THIhpvThAmK2n7itGI7bGGs7irEo3r7TG0R3LTnAIyOE+rYQf33HOyX2cMH
-M1SLFrD1I0173V3DcIIwIdU8ta38IfDJJJ/WAqHUNOwBdQ7aSYOjIZZDqyjcieKJ48K8ZBijgcxQ
-rHkqTqK5nAJDa3NyZDAosMwOyvXhm7DlMOPIzwdjhba6xJz9Gkk3u8lH1Bh4/941gfADqCfEbofV
-3ZgbX/aILs0oHHV8nEzqlrqlH3ImApzQq0okK4UbiTUfLTO8pfNUA5dK/MASBt7zy/eM91oSzWHd
-FS45dYtEGkNezoCikmPgYcNc6kAOspUukCWHdKMxn9B5guvxNES6GioqwVqrObXHwQwxgpT1x6ZC
-0iQJYyvB5K/dPktp08bm0Q5jRAqbH89Gun26/tOkDswcvVrng5rGwYk8lHyQ7d/WiR94yde6FOCX
-1R9o2pS5fUj+kqbuKfjRafTJg6jBVS55NpO8RLtnxoBkkGkvpAU/8En18wl9ikwL7Iy0XTDsvXdn
-dibE5uiF5RK7TTFmi2K2Vg3E9q7BIUI6EFDWx8lqXBF5tYkPmVsx4SOj9B9cbKxNbuJ4FtQru1MI
-UwPQ1c16F+N/5geqyrDMeg8tKJdawCzpTlBOTeEndRTzjtiSq48MKyzZ94EEA9sGbXmIYuKIizrf
-j/Oh1e4jbPKLauDGR/kvPA8kQGYn=
-<?php //000a3
-// IONCUBE ENCODER 13.0 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+namespace Coderstm\Providers;
 
-?>
-HR+cPw7E4QNMAkgAttY4XfFkb4RDhfBwZ7n2/978woaj1zxOB9n9KnKKfjWi6/3K7hAtNtoiMjQ7
-5OozDAAdtBFrW0vZKK9sx/0cNTwNMgMMRmCsYUPGfkGZ+emN9UDcjqah0AFdEsmuxDc4UPIP5dyz
-wqAJ34HDj9CUxQqN5S4hb+xWgh/3t7VY/vxi+HxE1q9OifEBy/mwLKyGSdb0rOw79Ek6TPSxxdmo
-L2tkiGoLoqPaMj82Lblwqvm71XMqe/UvseE11xX04xioOaq9NtB/q3wFXxauPe657gx4sIu3hHoC
-Uu4PSyIBEZPgw5bh/aS48MChRWmjDA5v3ER3nl6hHGIE4tRoU5JASn0pHavsctRtleecvmMpSeBa
-Fa9Jy0luguWkngG/wzjisHxNBqsIgYVcsW7/dlDXiZ5W8aSCwoXyer4+8UbMu7Yez3H0BENEIzv6
-+wbFygGgcYte1yVFQ1ur6/3xH4UCoNovkUzVpaHswBS41Mbp4HtH7ZfNswB4lpyrMx3BlJzVwnCV
-6Mc7RvYoOxK4jFkyw+2aAjvd6PflX9s97t7HIeeXNtdiVl0ogdwK6BUwQg07ptCfnHYj/LfvuqB/
-18ivuYiUwNAekUshO7hXgi1i3HncO0f1lEKSJvJZpyCUSXgGFSi0qV3tcxgamvkISTSJXbEEJaRn
-c8F+d3ElFLloYMsBCsCQBBNveHISzWHHxWkZQLxoGRP01W4++rQXgaKx7djQBuRPdl0mZpDaJ0Eg
-rtaHFVljOixHIHXORHOqaBy0r1X2nca913toOiELyKULPWr7WYJXiPN5Kmuke5+2+tViCgudXEGO
-60fnvo9Oz03DlVOzPXWgaq9EUBs5KSqRLNRHCqkOc5i4Zc827SurIi0cTG7KQZiQsZZyh1uHKEzC
-lxhXIJzUjKo2Hv5zB/qYh+iOLp7/+WdfhQT3Rzd2bzTD5J5/P8f+rdffBLJhobaQlwUmX1STcTPd
-mVTssUvuwFP+axY00QBD5y9E6nRftYyZNoh/dkzFT2gDcARvwqjZxwOMyUwMx65APsxQUYaUphfM
-+qAdXZEM4ufCeD3uxQKFvfFWRLMt4cDvM9W+kiIC395A1NAJHHi0vnxVosKZiRJbpCrjcaNILmWe
-WDH9WiaEdMHg/6r6O1DeUIwqWUGFKoQnh+lbnPmC8IkZfe7+myDlZWLo1B1UgRmMyMTQo8q4161g
-y3MWKlp6CE7iyk1Oe+Rz2ordfM2+Pnqqev/CD/qxXyAwYl+07MZ/OM1tDZVMMVu2K8iCzs0gDWrv
-WWJkGA88RVBsDIIY6SftzJ6GvcfdTzrijAugSEGmQMocZFssLJ8EJE6yOmyX0xsQa8jgqrMw6Vzm
-3Oq1Zq9wMpvj7v/cVxVeCIZhwZ+TJqP/8cAC+YdD1gFoa3tL2J1Z5YwpN0oHn0jqFeDVPl6QkLux
-TDJH2XZYQd/zSnFnHqDcOAmipKgY0eSY9sdmmtm0mNcuD3E9ztZ22DfQj/Yc0aTQx/+bqonXDLOO
-Ms/YjszC+OQwmJbJ5Vve37XHmXV336zjjJNMW3HHnxWPBdgv3Lleda95TM3y3+PAAvfRzRWAMEKn
-zvuIgvXWx/vYog4WyH5YbL76A5rSyzlwKTUtio1369xQYUBGC7kmGCnOVHYJiagwGWEEclJDc/Zw
-wea7LJZlKy05T0UaID9hAKaM3VkNv3cQo+yIQCHCurgrDzCSkQg1O1pa23+f12i0FQD6uKJb0KHp
-RBSkshdAU13jbV3vPLsoboLTWTtd4OiSsPHRismlI1wqw5MtHIak6JtlZeIEH7AikzC5g9OLMIt2
-wlhg3CG+nMyivswFT/a49H1baifWGJY4Fk2uB3PN5VrQ1fK1M3UKJDOGWwukonQVkptpi7Zx1I5A
-yo075+U0G7uzrC8aiQdv3XQAG2CF1TXf98Qy+ZYXW3LHLBLT38aRsWli0TbyjzJmzdjJfuOsIF2k
-WpUjichkEEqsZrnXobCkh/zw+7zoOVD9qp+HdCCWGQnkV+Q5eYcGKr9JR/gEkdALO37COrK1TI59
-aCccArl/lRn9baH1Q6hmhBoNHaFGW/RfrLeG/36bkeQur1Whau6m87qgd+9YWFxUP3j8uu5z58qv
-BwgDHgna+TtTk9fuhhYiL5T0vIDqz2+6QzQFqb1g/OM+D4meX/AfWvd6OngXc5Ap+jVs3uC3dTAl
-BFMXwLZI2+8YpvMoTMMnegRCjQ8Kp5x8zEZ0Yd2EbccOPgSbEFJ6qSJwrVn/tcxQrEflGRRKqX9s
-xZxnJHBYvD20fwSJU2WP0onTbI9V4vlDdAG9/TLn9FDTMarZqbTwALi915AbFHPKKgyDyprtdvHU
-zQW6A/5VDnGGDc2MoGZH4ps0rhIxq1lSOeTEmExFfX8X4l/PzcwmixLHXScw+OJnJgGqgPcQI1Ju
-HL4zmX6DGLphr//wNDE16tclHS3qpVDRUh2vBrdimf47+2XLdgS+crX8Uy4dyX0oLuDX1LUmXQma
-jA2iTe5ecUvBEWJYYxr7jbewQ4gGwAqh/6W3pAdNg3sSBn2JnzuOsnvHts4MjbnX11x7fUdkEKSk
-6lFyHuWCAyQQBejADWxs2sYpjyS6RZ/LxfKuRN/yWedTdSag5LA0LgDeTNxl1W+UUU48dlBHZI/1
-Y8eKi4XuAwNe1+st6R0dwOSH7+VG6hpDOnY/AijXy7Kz3Pbsj7DIIt7s4Igrus6VtCfdc+QbzCiC
-nL71tcWoiYMLMrcnTy5fwSa9zyG8RifKW+yD9ldS8Bt87RdZzNIT61fof8jzJl7k9PVW36VmDfKF
-WL8JxhaW4lnTKOt0hf1U48sD7BJtDQrmenh1uDf/1cMWEHCmdFAmVPylYQXpL8AzrsDEfKfAqF0F
-/r8g2ybamnGu7p31SnP6ESMDX67th13WSaFHoNBvablyAwGIf7hKqqA/kR2C4KBVU+XTySokeGeI
-D8aKrimn14TQaTKO+ckIQmPCPi4ZtW+WidAbYPHWrx+RwRxQ7aDFUBqg16fFqY1vzUwIsGlYmrL9
-yVjX4EdbBVkGSsJURqgWKKkntEQCnWUw9jTbWwyTIHzOhvO6wa345HMbQMtwc6LhbxQ7s6zEw4MW
-vCR6EqDE+Wqr4Ite8IsyJ+5UhZ7OEExLygKf1nDTzmt9+a8PwWt0e/c8gyXZKhZvGdnNInQ8Lrmn
-GUSiNWkS5YmXumAVA+jiL2P0+PJzJp2E1lKoLWTS4EyblkMRyrZ2Z/4qinjhdtZim3sWfwWuRGmD
-7ultSx17yRjLEGt25SAWEq361stKPdV9Q0NVY9kYEebhCvqQqQ2XV0XHZJIWL4ZWuidipdZbVDdY
-w48jsG/h4BlNQnuC
+use Coderstm\Models\Permission;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class CoderstmPermissionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        try {
+            Permission::get()->map(function ($permission) {
+                Gate::define($permission->scope, function ($user) use ($permission) {
+                    return $user->hasPermission($permission->scope);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            return false;
+        }
+
+        //Blade directives
+        Blade::directive('group', function ($group, $guard = 'users') {
+            return "if(guard() == {$guard} && current_user()->hasGroup({$group})) :"; //return this if statement inside php tag
+        });
+
+        Blade::directive('endgroup', function ($group) {
+            return "endif;"; //return this endif statement inside php tag
+        });
+    }
+}
