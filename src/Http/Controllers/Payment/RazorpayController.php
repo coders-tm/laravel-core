cfe1386182daf1a9970541d0d971fb535942b110
Dipak Sarkar <33681276+dipaksarkar@users.noreply.github.com>
2024-11-17 19:46:31 +0530

    Update RazorpayController.php

--- src/Http/Controllers/Payment/RazorpayController.php
+++ src/Http/Controllers/Payment/RazorpayController.php
@@ -103,7 +103,7 @@ class RazorpayController extends Controller
         ];
     }
 
-    public function wehbook(Request $request)
+    public function webhook(Request $request)
     {
         return response()->json([], 200);
     }
