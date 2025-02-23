d41ad736c5de773fa53bbf9793e36c6a1bead157
Dipak Sarkar <hello@dipaksarkar.in>
2025-02-23 13:07:59 +0530

    Upgrade

--- resources/views/shortcodes/contact-form.blade.php
+++ resources/views/shortcodes/contact-form.blade.php
@@ -3,11 +3,23 @@
     @csrf
     <input type="hidden" name="recaptcha_token" id="recaptcha_token">
     <div class="row ct-boxes-spacing-20px">
+        <div class="col-lg-12">
+            @if (session('success'))
+                <div id="form-success" class="alert alert-success" role="alert">
+                    {{ session('success') }}
+                </div>
+            @endif
+            @error('recaptcha_token')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
         <div class="col-md-6">
             <label>
                 <span class="text-input">
-                    <input class="@error('name') is-invalid @enderror" name="name" type="text" value=""
-                        placeholder="Name" required="required">
+                    <input class="@error('name') is-invalid @enderror" name="name" type="text"
+                        value="{{ old('name') }}" placeholder="Name" required="required">
                     @error('name')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -19,8 +31,8 @@
         <div class="col-md-6">
             <label>
                 <span class="text-input">
-                    <input class="@error('phone') is-invalid @enderror" name="phone" type="text" value=""
-                        placeholder="Phone Number" required="required">
+                    <input class="@error('phone') is-invalid @enderror" name="phone" type="text"
+                        value="{{ old('phone') }}" placeholder="Phone Number" required="required">
                     @error('phone')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -31,8 +43,8 @@
         <div class="col-md-12">
             <label>
                 <span class="text-input">
-                    <input class="@error('email') is-invalid @enderror" name="email" type="text" value=""
-                        placeholder="Email" required="required">
+                    <input class="@error('email') is-invalid @enderror" name="email" type="text"
+                        value="{{ old('email') }}" placeholder="Email" required="required">
                     @error('email')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -44,7 +56,7 @@
             <label>
                 <span class="text-input">
                     <textarea class="@error('message') is-invalid @enderror" name="message" rows="4"
-                        placeholder="Leave a comment or query" required="required"></textarea>
+                        placeholder="Leave a comment or query" required="required">{{ old('message') }}</textarea>
                     @error('message')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -59,10 +71,16 @@
                     type="submit">SEND NOW!</button>
             </div>
         </div>
-        <div class="col-lg-12">
-            <div id="form-success" style="display: none" class="alert alert-success" role="alert">
-                Message sent. We will contact you soon.
-            </div>
-        </div>
     </div>
 </form>
+<script src="https://www.google.com/recaptcha/api.js?render={{ config('recaptcha.site_key') }}"></script>
+<script>
+    var ReCaptchaCallbackV3 = function() {
+        grecaptcha.ready(function() {
+            grecaptcha.execute('{{ config('recaptcha.site_key') }}').then(function(token) {
+                document.getElementById("recaptcha_token").value = token;
+            });
+        });
+    };
+    ReCaptchaCallbackV3()
+</script>
